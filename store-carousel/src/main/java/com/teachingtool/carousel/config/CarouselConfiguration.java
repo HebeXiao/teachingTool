dec259e8560f389060c37366449b34c077b6f9f9
xiaoyihongbosssoft <xiaoyihong@bosssoft.com.cn>
2024-06-29 17:34:06 +0800

    29/06/2024 delete two services

--- store-carousel/src/main/java/com/teachingtool/carousel/config/CarouselConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.teachingtool.carousel.config;
-
-import com.teachingtool.config.CacheConfiguration;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class CarouselConfiguration extends CacheConfiguration {
-}
--- store-carousel/src/main/java/com/teachingtool/carousel/mapper/CarouselMapper.java
+++
@@ -1,7 +0,0 @@
-package com.teachingtool.carousel.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.teachingtool.pojo.Carousel;
-
-public interface CarouselMapper extends BaseMapper<Carousel> {
-}
